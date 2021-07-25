a7b686420bc44c89c4ef592b11cd8c7d38003a3f
Kyle Castillo <kyle.castillo90@gmail.com>
2021-07-25 16:23:55 -0400

    changed path in router

--- server.js
+++ server.js
@@ -49,7 +49,7 @@ app.use(methodOverride('_method'))// allow POST, PUT and DELETE from a form
 // Router
 //___________________
 const routerController = require('./controllers/router_controller.js')
-app.use('/', controller)
+app.use('/brokerage', controller)
 
 
 
