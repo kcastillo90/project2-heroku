d2a6bf47c2403260a5c369c4094ab38e84341250
Kyle Castillo <kyle.castillo90@gmail.com>
2021-07-26 17:51:53 -0400

    added brackets to currentUser line in user controller

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -4,7 +4,9 @@ const users = express.Router()
 const User = require('../models/users.js')
 
 users.get('/new', (req, res) => {
-  res.render('users/user_new.ejs', currentUser: req.session.currentUser)
+  res.render('users/user_new.ejs', {
+    currentUser: req.session.currentUser
+  })
 })
 
 users.post('/', (req, res) => {
