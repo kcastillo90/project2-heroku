42a22c889409568027f6617b53fe63f022c8372d
Kyle Castillo <kyle.castillo90@gmail.com>
2021-07-26 18:08:27 -0400

    moved mongodb lines to bottom of server.js in attempt to get sessions/new working

--- server.js
+++ server.js
@@ -21,16 +21,6 @@ const PORT = process.env.PORT || 3003
 // How to connect to the database either via heroku or locally
 const MONGODB_URI = process.env.MONGODB_URI
 
-// Connect to Mongo &
-// Fix Depreciation Warnings from Mongoose
-// May or may not need these depending on your Mongoose version
-mongoose.connect(MONGODB_URI , { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false }
-)
-
-// Error / success
-db.on('error', (err) => console.log(err.message + ' is Mongod not running?'))
-db.on('connected', () => console.log('mongo connected: ', MONGODB_URI))
-db.on('disconnected', () => console.log('mongo disconnected'))
 
 //___________________
 //Middleware
@@ -74,3 +64,14 @@ app.use('/', routerController)
 //Listener
 //___________________
 app.listen(PORT, () => console.log( 'Listening on port:', PORT))
+
+// Connect to Mongo &
+// Fix Depreciation Warnings from Mongoose
+// May or may not need these depending on your Mongoose version
+mongoose.connect(MONGODB_URI , { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false }
+)
+
+// Error / success
+db.on('error', (err) => console.log(err.message + ' is Mongod not running?'))
+db.on('connected', () => console.log('mongo connected: ', MONGODB_URI))
+db.on('disconnected', () => console.log('mongo disconnected'))
