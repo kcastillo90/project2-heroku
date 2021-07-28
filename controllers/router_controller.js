87514810a4fd35b67bc82179e5f15b64d6b5b51b
Kyle Castillo <kyle.castillo90@gmail.com>
2021-07-28 11:28:13 -0400

    removed home page stock list, fixed issues with routes working within user session, trying to get item deletions to be reflected in portfolio list

--- controllers/router_controller.js
+++ controllers/router_controller.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 const Stock = require('../models/stocks.js')
 const stockSeed = require('../models/seed.js')
-const UserStock = require('../models/users.js')   // attempt to get portfolio data from users.js
+const UserStock = require('../models/users.js')   // gets portfolio data from users.js
 
 const isAuthenticated = (req, res, next) => {
   if (req.session.currentUser) {
@@ -19,13 +19,11 @@ const isAuthenticated = (req, res, next) => {
 
 // Index for home page
 router.get('/' , (req, res) => {
-  Stock.find({}, (error, allStocks) => {
     res.render('home_index.ejs', {
-      stocks: allStocks,
       currentUser: req.session.currentUser
-    })
   })
 })
+
 
 // Add stock to user list form
 router.get('/users/addstock', (req, res) => {
@@ -83,29 +81,29 @@ router.get('/users/:id', (req, res) => {
 // })
 
 // Edit get
-router.get('/:id/edit', (req, res) => {
-  Stock.findById(req.params.id, (error, foundStock) => {
-    res.render(
-      'stock_edit.ejs',
-      {
-        stock: foundStock,
-        currentUser: req.session.currentUser
-      }
-    )
-  })
-})
-
-// Edit put
-router.put('/:id', isAuthenticated, (req, res) => {
-  Stock.findByIdAndUpdate(req.params.id, req.body, {new:true}, (error, updatedStock) => {
-    res.redirect(`/${req.params.id}`)
-  })
-})
+// router.get('/:id/edit', (req, res) => {
+//   Stock.findById(req.params.id, (error, foundStock) => {
+//     res.render(
+//       'stock_edit.ejs',
+//       {
+//         stock: foundStock,
+//         currentUser: req.session.currentUser
+//       }
+//     )
+//   })
+// })
+//
+// // Edit put
+// router.put('/:id', isAuthenticated, (req, res) => {
+//   Stock.findByIdAndUpdate(req.params.id, req.body, {new:true}, (error, updatedStock) => {
+//     res.redirect(`/${req.params.id}`)
+//   })
+// })
 
 // Delete
-router.delete('/:id', isAuthenticated, (req, res) => {
+router.delete('/:id', (req, res) => {
   Stock.findByIdAndRemove(req.params.id, (err, data) => {
-    res.redirect('/')
+    res.redirect(`/users/${req.session.currentUser.id}`)
   })
 })
 
