2bd81b61de06ad401327f891e452d45040117316
Kyle Castillo <kyle.castillo90@gmail.com>
2021-07-28 12:44:06 -0400

    got add, edit, delete routes all working for individual users, do not affect other users' portfolios

--- controllers/router_controller.js
+++ controllers/router_controller.js
@@ -57,56 +57,43 @@ router.get('/users/:id', (req, res) => {
   })
 })
 
-// Seed
-// router.get('/seed', (req, res) => {
-//   Stock.deleteMany({}, () => {})
-//   Stock.create(stockSeed, (error, data) => {
-//     error ? res.status(400).json(error) : res.status(200).json(data)
-//   })
-//   res.redirect('/')
-// })
-
-// // Get new for home
-// router.get('/new', (req, res) => {
-//   res.render('stock_new.ejs', {
-//       currentUser: req.session.currentUser
-//   })
-// })
-//
-// // Create new for home
-// router.post('/', isAuthenticated, (req, res) => {
-//   Stock.create(req.body, (error, addedStock) => {
-//     res.redirect('/')
-//   })
-// })
-
 // Edit get
-// router.get('/:id/edit', (req, res) => {
-//   Stock.findById(req.params.id, (error, foundStock) => {
-//     res.render(
-//       'stock_edit.ejs',
-//       {
-//         stock: foundStock,
-//         currentUser: req.session.currentUser
-//       }
-//     )
-//   })
-// })
-//
-// // Edit put
-// router.put('/:id', isAuthenticated, (req, res) => {
-//   Stock.findByIdAndUpdate(req.params.id, req.body, {new:true}, (error, updatedStock) => {
-//     res.redirect(`/${req.params.id}`)
-//   })
-// })
-
-// Delete
-router.delete('/:id', (req, res) => {
-  Stock.findByIdAndRemove(req.params.id, (err, data) => {
-    res.redirect(`/users/${req.session.currentUser.id}`)
+router.get('/:id/edit', (req, res) => {
+  Stock.findById(req.params.id, (error, foundStock) => {
+    res.render(
+      'stock_edit.ejs',
+      {
+        stock: foundStock,
+        currentUser: req.session.currentUser
+      }
+    )
   })
 })
 
+// Edit put
+router.put('/:id', (req, res) => {
+  Stock.findByIdAndUpdate(req.params.id, req.body, {new:true}, (error, updatedStock) => {
+    UserStock.findOne({ 'portfolio._id': req.params.id }, (err, foundUser) => {
+      foundUser.portfolio.id(req.params.id).remove()
+      foundUser.portfolio.push(updatedStock)
+      foundUser.save( (err, data) => {
+        res.redirect(`/${req.params.id}`)
+      })
+    })
+  })
+})
+
+// Delete
+router.delete('/:id', (req, res) => {
+  UserStock.findByIdAndRemove(req.params.id, (err, foundStock) => {
+    UserStock.findOne({'portfolio._id': req.params.id}, (err, foundUser) => {
+      foundUser.portfolio.id(req.params.id).remove()
+      foundUser.save( (err, data) => {
+        res.redirect(`/users/${req.session.currentUser.id}`)
+      })
+    })
+  })
+})
 
 // Show
 router.get('/:id', (req, res) => {
